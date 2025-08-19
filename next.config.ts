a6e0849d83ab01598923e0dcc8b03f1a61462c85
ahmedooode <ahmednasrehab2019@gmail.com>
2025-08-19 16:01:05 +0300

    Skip authentication for API routes that handle their own auth

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
