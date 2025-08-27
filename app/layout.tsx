ccacfa149f1293f6b001fab826a42929ff5ba107
ahmedooode <ahmednasrehab2019@gmail.com>
2025-08-27 12:09:54 +0300

    Complete teacher control panel and full teacher experience, except sales analysis

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToasterProvider } from "@/components/providers/toaster-provider";
+import ConfettiProvider from "@/components/providers/confetti-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClerkProvider>
+          <ConfettiProvider />
           <ToasterProvider />
           {children}
         </ClerkProvider>
