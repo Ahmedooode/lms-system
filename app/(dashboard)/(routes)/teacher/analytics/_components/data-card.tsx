08ce5257b7f90a06b7a10e2d3c7916e56ab3ffef
ahmedooode <ahmednasrehab2019@gmail.com>
2025-09-11 14:34:46 +0300

    all main functions in the project are compeleted in this commit

---
+++ app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx
@@ -0,0 +1,23 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { formatPrice } from "@/lib/format";
+
+interface DataCardProps {
+  value: number;
+  label: string;
+  shouldFormat?: boolean;
+}
+
+export const DataCard = ({ value, label, shouldFormat }: DataCardProps) => {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">{label}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+          {shouldFormat ? formatPrice(value) : value}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -3,13 +3,14 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 
 import { NextResponse } from "next/server";
+import { isTeacher } from "@/lib/teacher";
 
 export async function POST(req: Request) {
   try {
     const { userId } = await auth();
     const { title } = await req.json();
 
-    if (!userId) {
+    if (!userId || !isTeacher(userId)) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
     const course = await db.course.create({
